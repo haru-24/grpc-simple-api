bf52bf90da7a27a46a7d6a7ab5acabc459bbcc8a
Haru Nishiura <haru.nishiura@nextremer.com>
2022-10-19 23:41:48 +0900

    api function completed

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # grpc-sample-api
+
+## Run envoy
+image build
+```bash
+$ sudo docker build -t envoy .
+```
+run container
+```bash
+$ docker run -it -p 8080:8080 envoy
+```
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { GreeterPromiseClient } from "./helloworld/proto/helloworld_grpc_web_pb";
+import { HelloRequest } from "./helloworld/proto/helloworld_pb";
 
 function App() {
+  const [msg, setMsg] = useState("")
+
+  const client = new GreeterPromiseClient("http://localhost:8080", null, null)
+
+
+  const onClickRequest = () => {
+    const request = new HelloRequest()
+    request.setName("gRPC!!!!")
+    client.sayHello(request).then((reply) => {
+      console.log(reply.getMessage())
+      setMsg(reply.getMessage())
+    })
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -10,8 +25,8 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        こんにちはJAPAN
-        <p></p>
+        <button onClick={onClickRequest}>gRPC</button>
+        <p>{msg}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
